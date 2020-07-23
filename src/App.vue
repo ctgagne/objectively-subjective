55c790e99ef4ad459d65538b61a36788515754eb
Chaz Gagne <chazgagne@gmail.com>
2020-07-23 14:00:38 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
-
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400;1,500;1,700&family=Titan+One&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0c0e0d;
 }
 
-#nav {
-  padding: 30px;
+:root {
+  --light-text: #ccc9a8;
+  --dark-text: #0c0e0d;
+  --light-background: #ccc9a8;
+  --dark-background: #cf6f33;
+  --primary-color: #cf6f33;
+  --component-color: #669358;
+  --component-background: #c4791033;
+  --accent-dark: #6e3b3b;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  margin: 0 auto;
+  background-color: #cf6f33;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="main-section">
+      <div class="home-text">
+        <div class="title">The Objectively Subjective Podcast</div>
+        <h1>
+          A podcast where we <span class="highlight">objectively</span> give you
+          our <span class="highlight">subjective </span><br /><u>Top 5 lists</u>
+        </h1>
+      </div>
+      <img alt="OS logo" src="../assets/objectively-subjective.jpg" />
+    </div>
+    <div class="player">
+      <div class="listen">
+        <p>Listen to our most recent episode!</p>
+      </div>
+      <iframe
+        height="150px"
+        width="96%"
+        frameborder="no"
+        scrolling="no"
+        seamless
+        src="https://player.simplecast.com/b048daef-0db1-4dc0-869a-867c70b62139?dark=true"
+      ></iframe>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
+
+<style scoped>
+.main-section {
+  display: flex;
+  max-width: 1440px;
+  margin: 0 auto;
+  margin-top: -2.5rem;
+}
+.title {
+  font-weight: bold;
+  font-style: italic;
+  font-size: 2rem;
+  text-align: left;
+  padding-left: 1rem;
+}
+
+h1 {
+  font-family: "Titan One", Arial, Helvetica, sans-serif;
+  font-size: 72px;
+  text-shadow: 0px 7px 4px rgba(0, 0, 0, 0.25);
+  text-align: left;
+  margin-top: 0;
+  padding: 0 1rem;
+}
+
+.highlight {
+  color: var(--light-text);
+}
+
+.home-text {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 2rem;
+}
+.home img {
+  width: 650px;
+  height: 650px;
+}
+
+.player {
+  display: flex;
+  flex-direction: column;
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 1rem;
+  margin-top: -2.5rem;
+}
+
+.player p {
+  padding-left: 1rem;
+  text-align: left;
+}
+
+iframe {
+  padding: 0 1rem;
+}
+</style>
