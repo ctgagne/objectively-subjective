672fe28029741fb12bd52be80856aba925625a70
Chaz Gagne <chazgagne@gmail.com>
2020-07-29 14:14:13 +0200

    add episode guide view

---
+++ src/views/EpisodeGuide.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div id="audio" class="player-wrapper">
+      <audio-player
+        file="https://upload.wikimedia.org/wikipedia/commons/d/d6/Louis-Emmanuel_Jadin_-_Nocturne_No._3_in_G_minor_-_2._Allegro_molto.ogg"
+      ></audio-player>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style scoped></style>
