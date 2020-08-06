3437a3da2bff11ceea60128f3d70e5819b39c42a
Chaz Gagne <chazgagne@gmail.com>
2020-08-06 12:57:32 +0200

    inline-flex

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,17 +6,17 @@
           href="https://open.spotify.com/show/2uu2Xzjdc3Ur2jnwnfhEgF?si=C4sw7sN-SDWSNtynKSqwbQ"
           ><button class="spotify">
             <svg
+              class="spotify-icon"
               xmlns="http://www.w3.org/2000/svg"
-              width="20px"
-              viewBox="0 0 496 512"
+              viewBox="0 0 512 512"
+              aria-hidden="true"
+              focusable="false"
             >
               <path
                 d="M248 8C111.1 8 0 119.1 0 256s111.1 248 248 248 248-111.1 248-248S384.9 8 248 8zm100.7 364.9c-4.2 0-6.8-1.3-10.7-3.6-62.4-37.6-135-39.2-206.7-24.5-3.9 1-9 2.6-11.9 2.6-9.7 0-15.8-7.7-15.8-15.8 0-10.3 6.1-15.2 13.6-16.8 81.9-18.1 165.6-16.5 237 26.2 6.1 3.9 9.7 7.4 9.7 16.5s-7.1 15.4-15.2 15.4zm26.9-65.6c-5.2 0-8.7-2.3-12.3-4.2-62.5-37-155.7-51.9-238.6-29.4-4.8 1.3-7.4 2.6-11.9 2.6-10.7 0-19.4-8.7-19.4-19.4s5.2-17.8 15.5-20.7c27.8-7.8 56.2-13.6 97.8-13.6 64.9 0 127.6 16.1 177 45.5 8.1 4.8 11.3 11 11.3 19.7-.1 10.8-8.5 19.5-19.4 19.5zm31-76.2c-5.2 0-8.4-1.3-12.9-3.9-71.2-42.5-198.5-52.7-280.9-29.7-3.6 1-8.1 2.6-12.9 2.6-13.2 0-23.3-10.3-23.3-23.6 0-13.6 8.4-21.3 17.4-23.9 35.2-10.3 74.6-15.2 117.5-15.2 73 0 149.5 15.2 205.4 47.8 7.8 4.5 12.9 10.7 12.9 22.6 0 13.6-11 23.3-23.2 23.3z"
               />
             </svg>
-            <h4>
-              Follow on Spotify
-            </h4>
+            <span>Follow on Spotify</span>
           </button></a
         >
       </div>
@@ -70,24 +70,31 @@ export default {};
 }
 
 button.spotify {
-  display: flex;
   cursor: pointer;
+  width: max-content;
   background-color: whitesmoke;
   border: none;
   border-radius: 0.5rem;
   height: 40px;
-  place-items: center;
-  justify-content: center;
-  align-items: center;
   margin-bottom: 4px;
-  padding: 0.5rem 0.9rem;
+  padding: 0.5rem 1rem;
+  font-weight: bold;
+  color: var(--dark-text);
+  fill: var(--dark-text);
+  display: inline-flex;
+  vertical-align: middle;
 }
 
 button.spotify:hover {
   opacity: 0.8;
 }
 
+.spotify span {
+  padding-top: 4px;
+}
+
 button svg {
+  width: 20px;
   padding-right: 4px;
 }
 
